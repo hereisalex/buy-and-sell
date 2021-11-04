34d13db106aff43c0ea77255b095bd701a0b1919
hereisalex <67809769+hereisalex@users.noreply.github.com>
2021-11-04 00:12:12 -0500

    implement firebase auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
  
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +15,7 @@ import { NewListingPageComponent } from './new-listing-page/new-listing-page.com
 import { EditListingPageComponent } from './edit-listing-page/edit-listing-page.component';
 import { ListingDataFormComponent } from './listing-data-form/listing-data-form.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { environment } from 'environments/environment';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,18 @@
 // This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC8NnowY-_vAbYNZ2hwTGebTmuqAAfFwMI",
+    authDomain: "buy-and-sell-12345.firebaseapp.com",
+    projectId: "buy-and-sell-12345",
+    storageBucket: "buy-and-sell-12345.appspot.com",
+    messagingSenderId: "370427286994",
+    appId: "1:370427286994:web:6a8548bcfa1a9bf7a2e40b",
+    measurementId: "G-2V8MQSBH5M"
+  }
 };
 
 /*
@@ -13,4 +22,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
